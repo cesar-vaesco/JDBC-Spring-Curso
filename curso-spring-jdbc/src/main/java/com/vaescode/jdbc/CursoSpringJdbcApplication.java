ef0d6feee726647b01b28a447c76d56421f0618b
Cesar <cesarvargasescorcia@gmail.com>
2020-11-05 09:23:22 -0600

    JDBC:  template de jdbc inyeccción con @Autowired

--- curso-spring-jdbc/src/main/java/com/vaescode/jdbc/CursoSpringJdbcApplication.java
+++ curso-spring-jdbc/src/main/java/com/vaescode/jdbc/CursoSpringJdbcApplication.java
@@ -2,28 +2,31 @@ package com.vaescode.jdbc;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class CursoSpringJdbcApplication {
+public class CursoSpringJdbcApplication implements ApplicationRunner {
 
-	
 	private static final Logger log = LoggerFactory.getLogger(CursoSpringJdbcApplication.class);
-	
-	
 
-	public static void main(String[] args) {
-		
-		ConfigurableApplicationContext context = SpringApplication.run(CursoSpringJdbcApplication.class, args);
+	@Autowired
+	private JdbcTemplate template;
 
-		JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		// TODO Auto-generated method stub
 
-		
 	}
 
-	
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringJdbcApplication.class, args);
+
+	}
 
 }
