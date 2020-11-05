d856f08c9b9b9c2d92c63805e10a849f65d13b54
Cesar <cesarvargasescorcia@gmail.com>
2020-11-05 09:19:49 -0600

    JDBC: ejemplo de como invocar el template de jdbc

--- curso-spring-jdbc/src/main/java/com/vaescode/jdbc/CursoSpringJdbcApplication.java
+++ curso-spring-jdbc/src/main/java/com/vaescode/jdbc/CursoSpringJdbcApplication.java
@@ -1,25 +1,29 @@
 package com.vaescode.jdbc;
 
-import javax.sql.DataSource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class CursoSpringJdbcApplication {
 
 	
 	private static final Logger log = LoggerFactory.getLogger(CursoSpringJdbcApplication.class);
+	
+	
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(CursoSpringJdbcApplication.class, args);
 		
-		DataSource dataSource = context.getBean(DataSource.class);
-		log.info("DataSource implementation {}", dataSource.getClass().getName());
+		ConfigurableApplicationContext context = SpringApplication.run(CursoSpringJdbcApplication.class, args);
 
+		JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
+
+		
 	}
+
+	
 
 }
