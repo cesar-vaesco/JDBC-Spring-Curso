514649fdc40f7d05ce0492e125cc36bab52336a6
Cesar <cesarvargasescorcia@gmail.com>
2020-11-04 19:32:47 -0600

    JDBC: Creando y probando el pool de conexiones - velocidad de conexión

--- Curso_jdbc/src/main/java/com/vaescode/jdbc/datasource/ExcerciseConnection.java
+++ Curso_jdbc/src/main/java/com/vaescode/jdbc/datasource/ExcerciseConnection.java
@@ -1,30 +1,36 @@
 package com.vaescode.jdbc.datasource;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.h2.jdbcx.JdbcConnectionPool;
+
 public class ExcerciseConnection {
 
 	private static final int NUM_CONNECTIONS = 100;
-
+	/*
+	 * Velocidad de conexión        Usando pool de conexiones
+	 * 
+	 *   1 = 480ms                    1 = 259ms     
+	 *   5 = 1892ms                   5 = 260ms
+	 *  10 = 3348ms                  10 = 224ms
+	 *  20 = 7169ms                  20 = 259ms
+	 *  30 = 10906ms                 30 = 260ms
+	 *  50 = 14984ms                 50 = 260ms
+	 * 100 = 28898ms                100 = 269ms
+	 * */
 	public static void main(String[] args) throws SQLException {
+		
+		JdbcConnectionPool connectionPool = JdbcConnectionPool.create("jdbc:h2:~/test","","");
 		long startTime = System.currentTimeMillis();
 		
-		/*
-		 * Velocidad de conexión
-		 * 
-		 *   1 = 480ms
-		 *   5 = 1892ms
-		 *  10 = 3348ms
-		 *  20 = 7169ms
-		 *  30 = 10906ms
-		 *  50 = 14984ms
-		 * 100 = 28898ms
-		 * */
-		for (int i = 0; i < NUM_CONNECTIONS; i++) {
-			Connection connection = DriverManager.getConnection("jdbc:h2:~/test");
-			
+		
+		for (int i = 1; i <= NUM_CONNECTIONS; i++) {
+			Connection connection =  connectionPool.getConnection();
+
+			System.out.println(i);
 			connection.close();
 		}
 		
